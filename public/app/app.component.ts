bee7a5dde8d12a9bdabf8e0f4351626bb57fbd0e
Karthikeyan Nallathambi <karthikeyan.nallathambi@gmail.com>
2017-01-22 22:48:04 +0530

    Initial version

---
+++ public/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'starter-app',
+  template: `
+  <home-panel></home-panel>
+  `,
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ public/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-panel',
+  templateUrl: 'app/home/home.component.html'
+
+})
+export class HomeComponent  {
+
+  private activeTabIndex:number = 0;
+
+}
